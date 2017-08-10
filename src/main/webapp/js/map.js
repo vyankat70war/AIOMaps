1efce61a7ffaa3baacc5d634704646d3f59fab5a
Vyankat Sattarwar <vyankat.sattarwar@tomtom.com>
2017-08-10 10:41:18 +0530

    added traffic layer

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -3,11 +3,12 @@ var map = L
 				"map",
 				{
 					key : "b5w0ip0dC0PPuyZ75hbmUPBcQK7IhO0V",
-					source : "vector",
+					 source: ["raster", "vector"],
 					glyphsUrl : "https://api.tomtom.com/maps-sdk-js/glyphs/v1/{fontstack}/{range}.pbf",
 					spriteUrl : "https://api.tomtom.com/maps-sdk-js/sprites/v1/sprite",
 					preferCanvas : true,
-					inertia : false
+					inertia : false,
+					traffic: { style: "s3", key: "b5w0ip0dC0PPuyZ75hbmUPBcQK7IhO0V" }
 				}, 10);
 
 map.locate({setView: true, maxZoom: 15});
@@ -15,13 +16,18 @@ map.locate({setView: true, maxZoom: 15});
 /*
  * Added Reports layer 
  */
-var imported = document.createElement('script');
+/*var imported = document.createElement('script');
 imported.src = 'js/ReportsLayer.js';
-document.head.appendChild(imported);
+document.head.appendChild(imported);*/
 
-
+/*
+ * Location search layer
+ */
 var imported = document.createElement('script');
 imported.src = 'js/SearchLayer.js';
 document.head.appendChild(imported);
+
+
+
 
 
